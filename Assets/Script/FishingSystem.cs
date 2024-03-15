f4fae5f0cf1221d00a7bebf99303a365cd83b629
jws2170 <mpart2077@gmail.com>
2024-03-16 00:51:21 +0900

    [FIX] FishingSystem

--- Assets/Script/FishingSystem.cs
+++ Assets/Script/FishingSystem.cs
@@ -6,23 +6,44 @@ using UnityEngine.UI;
 public class FishingSystem : MonoBehaviour
 {
     [SerializeField] private Button FishingBt;
-    public float FishHP;
+    [SerializeField] private Text FishingBtText;
+    public float MaxFishHp;
+    public float CurrentFishHp;
+    public float FishRobHp;
     public float FishRobPower;
+    public bool Ishrowing;
 
     // Start is called before the first frame update
     void Start()
     {
+        Ishrowing = false;
+        CurrentFishHp = MaxFishHp;
         FishingBt.onClick.AddListener(Fishing);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void Fishing()
     {
-        FishHP -= FishRobPower;
+        if(Ishrowing)
+        {
+            CurrentFishHp -= FishRobPower;
+            if (CurrentFishHp == 0)
+            {
+                FishingBtText.text = "던지기";
+                CurrentFishHp = MaxFishHp;
+                Ishrowing = false;
+            }
+        }
+        else
+        {
+            FishingBtText.text = "잡기";
+            Ishrowing = true;
+        }
+
     }
 }
