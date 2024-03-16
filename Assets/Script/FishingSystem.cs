c0e38936fb81e75120e0805854d3b15c1b874724
jws2170 <mpart2077@gmail.com>
2024-03-16 20:39:13 +0900

    [ADD] TocuhSystem

--- Assets/Script/FishingSystem.cs
+++ Assets/Script/FishingSystem.cs
@@ -2,48 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class FishingSystem : MonoBehaviour
 {
-    [SerializeField] private Button FishingBt;
-    [SerializeField] private Text FishingBtText;
     public float MaxFishHp;
     public float CurrentFishHp;
     public float FishRobHp;
     public float FishRobPower;
     public bool Ishrowing;
+    private bool IsFocus;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Ishrowing = false;
         CurrentFishHp = MaxFishHp;
-        FishingBt.onClick.AddListener(Fishing);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Fishing();
     }
 
     void Fishing()
     {
-        if(Ishrowing)
+        if(IsFocus == true)
         {
-            CurrentFishHp -= FishRobPower;
-            if (CurrentFishHp == 0)
+            if (Input.touchCount > 0)
             {
-                FishingBtText.text = "던지기";
-                CurrentFishHp = MaxFishHp;
-                Ishrowing = false;
+                for(int i = 0; i < Input.touchCount; i++)
+                {
+                    if (EventSystem.current.IsPointerOverGameObject(i) == false)
+                    {
+                        Touch touch = Input.GetTouch(0);
+                        if (touch.phase == TouchPhase.Began)
+                        {
+                            if (Ishrowing)
+                            {
+                                CurrentFishHp -= FishRobPower;
+                                if (CurrentFishHp == 0)
+                                {
+                                    CurrentFishHp = MaxFishHp;
+                                    Ishrowing = false;
+                                }
+                            }
+                            else
+                            {
+                                Ishrowing = true;
+                            }
+                        }
+                    }
+                }
+
             }
         }
-        else
-        {
-            FishingBtText.text = "잡기";
-            Ishrowing = true;
-        }
+    }
 
+    private void OnApplicationFocus(bool focus)
+    {
+        IsFocus = focus;
     }
 }
